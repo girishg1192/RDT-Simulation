f535fd802f2439817f54302e5015fcc87582a947
Girish Gokul <girishg1192@gmail.com>
2016-03-25 20:33:09 -0400

    GBN: Message buffering at sender

---
+++ g8/include/gbn.h
@@ -0,0 +1,28 @@
+#ifndef __SIMULATOR__
+#include "../include/simulator.h"
+#define __SIMULATOR__
+#include <stdlib.h>
+#endif
+
+#include "rdt_lib.h"
+#include "list_lib.h"
+
+int A_base;
+int A_nextseqnum;
+int window_size;
+int is_timer_running;
+int buff_count;
+
+int B_expectedseq;
+struct pkt B_last_ack;
+
+struct list current_window;
+struct list buffered_packets;
+
+struct packet_elem
+{
+  struct pkt packet;
+  struct list_elem elem;
+};
+
+#define TIMER_EXPIRE 20
--- g8/src/gbn.c
+++ g8/src/gbn.c
@@ -1,5 +1,5 @@
-#include "../include/simulator.h"
-#include "rdt_lib.h"
+#include "gbn.h"
+#include <stdio.h>
 
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
@@ -16,11 +16,8 @@
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
-#define TIMER_EXPIRE 10
-int A_base;
-int A_nextseqnum;
-int window_size;
-int is_timer_running;
+#define MAX_BUFFER 50
+
 void check_and_start_timer()
 {
   if(A_base == A_nextseqnum && !is_timer_running)
@@ -34,11 +31,15 @@ void restart_timer()
   if(is_timer_running)
     stoptimer(A);
   starttimer(A, TIMER_EXPIRE);
+  is_timer_running = 1;
 }
 void check_and_stop_timer()
 {
   if(is_timer_running)
+  {
     stoptimer(A);
+    is_timer_running = 0;
+  }
 }
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(message)
@@ -46,11 +47,29 @@ void A_output(message)
 {
   if(A_nextseqnum < (A_base+window_size))
   {
-    struct pkt data = make_packet(A_nextseqnum, 0, message.data);
-    //Add data to list
-    tolayer3(A, data);
+    struct packet_elem *curr_data = malloc(sizeof(struct packet_elem));
+    curr_data->packet = make_packet(A_nextseqnum, 0, message.data);
+    list_push_back(&current_window, &curr_data->elem);
+    tolayer3(A, curr_data->packet);
+    printf("Packet: %d %f\n", A_nextseqnum, get_sim_time());
     check_and_start_timer();
     A_nextseqnum++;
+  }
+  else
+  {
+    printf("RDT_SEND:Buffer %d\n", buff_count);
+    if(buff_count< MAX_BUFFER)
+    {
+      struct packet_elem *buff_data = malloc(sizeof(struct packet_elem));
+      printf("RDT_SEND:alloc\n");
+      int buffered_sequence = A_nextseqnum + buff_count;
+      buff_data->packet = make_packet(buffered_sequence, 0, message.data);
+      printf("RDT_SEND:alloc\n");
+      list_push_back(&buffered_packets, &buff_data->elem);
+      printf("RDT_SEND:push\n");
+      buff_count++;
+    }
+    printf("RDT_SEND: End");
   }
 }
 
@@ -58,20 +77,49 @@ void A_output(message)
 void A_input(packet)
   struct pkt packet;
 {
+
   if(!isCorrupted(&packet))
   {
     A_base = packet.acknum + 1;
+    printf("Packet: %d %f\n", packet.acknum, get_sim_time());
+    for(struct list_elem *e = list_begin(&current_window); 
+        e!=list_end(&current_window) && list_entry(e, struct packet_elem, elem)->packet.seqnum < A_base;
+        e = list_remove(e));
+    send_buffered();
     if(A_base == A_nextseqnum)
       check_and_stop_timer();
     else
       restart_timer();
+  }
+  else
+    printf("ACK: Corrupt\n");
+}
+send_buffered()
+{
+  while(A_nextseqnum< (A_base+window_size) && buff_count)
+  {
+    struct pkt buff_data = list_entry(list_pop_front(&buffered_packets), 
+           struct packet_elem, elem)->packet;
+    tolayer3(A, buff_data);
+    A_nextseqnum++;
+    buff_count--;
   }
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
+  printf("TIMERINT start\n");
+  starttimer(A,TIMER_EXPIRE);
+  is_timer_running = 1;
   //Send all data in list
+  for(struct list_elem *e = list_begin(&current_window); e!=list_end(&current_window);
+      e = list_next(e))
+  {
+    struct packet_elem *sndpkt= list_entry(e, struct packet_elem, elem);
+    tolayer3(A, sndpkt->packet);
+  }
+  printf("TIMERINT End\n Send %d frames\n", A_nextseqnum - A_base);
 }  
 
 /* the following routine will be called once (only) before any other */
@@ -80,22 +128,26 @@ void A_init()
 {
   A_base = 1;
   A_nextseqnum = 1;
+  buff_count = 0;
+  is_timer_running = 0;
+
   window_size = getwinsize();
+  list_init(&current_window);
+  list_init(&buffered_packets);
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
 //===================B code ========================//
-int B_expectedseq;
-struct pkt B_last_ack;
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(packet)
   struct pkt packet;
 {
-  if(!isCorrupted(packet) && packet.seqnum == B_expectedseq)
+  if(!isCorrupted(&packet) && packet.seqnum == B_expectedseq)
   {
     tolayer5(B, packet.payload);
     B_last_ack = make_packet(0, B_expectedseq, NULL);
     tolayer3(B, B_last_ack);
+    B_expectedseq++;
   }
 }
 
